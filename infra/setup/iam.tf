504792b9049d5df8835087bcf3545f54da4f12f0
Derek <Derek@euc.life>
2024-06-07 11:25:44 -0700

    added rds

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -167,7 +167,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance",
+      "rds:ModifyDBInstance"
     ]
     resources = ["*"]
   }
